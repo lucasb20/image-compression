a92b2b09eecf6f7ad8338adefcd66a8fa9ee6655
Lucas Rocha <lr359508@gmail.com>
2024-10-05 15:22:18 -0300

    feat: função de critério de homogeneidade criada

---
+++ funcs.c
@@ -0,0 +1,21 @@
+#include "lib/funcs.h"
+#include <math.c>
+
+int criterion(struct Image img){
+    int mean = 0;
+    int n = img.height * img.width;
+    for (int i = 0; i < img.height * img.width; i++){
+        mean += img.Data[i];
+    }
+    mean /= img.height * img.width;
+    int var = 0;
+    for (int i = 0; i < img.height * img.width; i++){
+        int dif = img.Data[i] - mean;
+        var += dif*dif;
+    }
+    var /= img.height * img.width - 1;
+    return sqrt(var) < 10 ? 0 : var;
+}
+
+void encoder(){}
+void decoder(){}
---
+++ lib/funcs.h
@@ -0,0 +1,3 @@
+#include "pgm.h"
+
+int criterion(struct Image);
