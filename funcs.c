825a20ff02aa4b06d20678bfa07ed53ef5a857de
Lucas Rocha <lr359508@gmail.com>
2024-10-06 07:02:26 -0300

    style: função de codificação mostrada primeiro

--- funcs.c
+++ funcs.c
@@ -2,22 +2,6 @@
 #include "lib/funcs.h"
 #include "lib/pgm.h"
 
-int criterion(struct Image img){
-    int mean = 0;
-    int n = img.height * img.width;
-    for (int i = 0; i < img.height * img.width; i++){
-        mean += img.Data[i];
-    }
-    mean /= img.height * img.width;
-    int var = 0;
-    for (int i = 0; i < img.height * img.width; i++){
-        int dif = img.Data[i] - mean;
-        var += dif*dif;
-    }
-    var /= img.height * img.width - 1;
-    return sqrt(var) < 10 ? mean : 0;
-}
-
 void encoder(char* filename){
     struct Image src, des;
     readPGMImage(&src, filename);
@@ -34,5 +18,22 @@ void encoder(char* filename){
 }
 
 void decoder(char* filename){}
+
+int criterion(struct Image img){
+    int mean = 0;
+    int n = img.height * img.width;
+    for (int i = 0; i < img.height * img.width; i++){
+        mean += img.Data[i];
+    }
+    mean /= img.height * img.width;
+    int var = 0;
+    for (int i = 0; i < img.height * img.width; i++){
+        int dif = img.Data[i] - mean;
+        var += dif*dif;
+    }
+    var /= img.height * img.width - 1;
+    return sqrt(var) < 10 ? mean : 0;
+}
+
 void divideByCriterion(struct Image *src, struct Image *des, int low_width, int high_width, int low_height, int high_height){};
 void writeBitstream(struct Image *img, char* filename){};
